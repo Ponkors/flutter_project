765b9e8d3c901c3223473da20cec47c9c891b2f7
Vlad Kirichuk <vlad.kirichuk50@gmail.com>
2023-07-21 13:05:39 +0300

    [PT-1] Firebase login, and files configuration for all platforms.

--- navigation/lib/src/app_router/app_router.dart
+++ navigation/lib/src/app_router/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
-
 import 'package:home/home.dart';
 
 part 'app_router.gr.dart';
@@ -8,9 +7,21 @@ part 'app_router.gr.dart';
 @MaterialAutoRouter(
   replaceInRouteName: 'Screen,Route',
   routes: <AutoRoute>[
-    AutoRoute(page: WelcomeScreen, initial: true),
-    AutoRoute(page: WalkthroughScreenView),
-    AutoRoute(page: SingleRestaurantScreen),
+    AutoRoute(
+        page: WelcomeScreen,
+        // path: 'welcome_screen.dart',
+        name: 'WelcomeScreen',
+        initial: true),
+    AutoRoute(
+      page: WalkthroughScreenView,
+      // path: 'walkthrough_screen.dart',
+      name: 'WalkthroughScreen',
+    ),
+    AutoRoute(
+      page: SingleRestaurantScreen,
+      // path: 'single_restaurant_screen',
+      name: 'SingleRestaurantScreen',
+    ),
   ],
 )
 class AppRouter extends _$AppRouter {}
