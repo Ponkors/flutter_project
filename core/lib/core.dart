48e57c45a6f055a80a7808d20777681faf199a24
Vlad Kirichuk <vlad.kirichuk50@gmail.com>
2023-07-25 20:35:29 +0300

    [PT-1] Upload firebase components.

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,6 +1,8 @@
 library core;
 
-// export 'package:firebase_core/firebase_core.dart';
-// export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:get_it/get_it.dart';
 
 export 'package:core/di/app_di.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:core/core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:features/features.dart';
 import 'package:food_app/app/flutter_app.dart';
 import 'package:core/config/firebase_options.dart';
 
