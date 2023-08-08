1fb6d140147ad9cfee3011124c1b5cf50ad765d4
Vladislav Kirichuk <vlad.kirichuk50@gmail.com>
2023-08-08 16:26:52 +0300

    [PT-1] Create menu_item_model.freezed.dart and menu_item_model.g.dart files.

---
+++ data/lib/models/menu_item_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'menu_item_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_MenuItemModel _$$_MenuItemModelFromJson(Map<String, dynamic> json) =>
+    _$_MenuItemModel(
+      image: json['image'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$$_MenuItemModelToJson(_$_MenuItemModel instance) =>
+    <String, dynamic>{
+      'image': instance.image,
+      'name': instance.name,
+    };
